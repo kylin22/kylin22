83b3360f411e5bf47627de3049a69968740dc5d2
Kylin <107255387+kylin22@users.noreply.github.com>
2024-12-22 15:55:31 +1100

    fix: remove angle threshold and simply increased lerp speed to minimize flanging

--- src/composables/squishyCursor.ts
+++ src/composables/squishyCursor.ts
@@ -3,7 +3,6 @@ import { Vector2 } from "three";
 export const useSquishyCursor = (element: HTMLElement) => {
   const STRETCH_FACTOR = 0.3;
   const STRETCH_VELOCITY = 2;
-  const ANGLE_THRESHOLD = 60;
 
   const cursor = document.createElement("div");
   cursor.id = "cursor";
@@ -12,7 +11,6 @@ export const useSquishyCursor = (element: HTMLElement) => {
 
   let mousePosition = new Vector2(0, 0);
   let lastMousePosition = new Vector2(0, 0);
-  let lastAngle = 0;
   let cursorPosition = new Vector2(0, 0);
   let velocity = new Vector2(0, 0);
 
@@ -24,20 +22,16 @@ export const useSquishyCursor = (element: HTMLElement) => {
     velocity = new Vector2().copy(mousePosition).sub(lastMousePosition);
     lastMousePosition = new Vector2().copy(mousePosition);
 
-    cursorPosition.lerp(mousePosition, 0.5);
+    cursorPosition.lerp(mousePosition, 0.8);
     
-    let angle = new Vector2().copy(cursorPosition).sub(mousePosition).angle() * (180 / Math.PI);
+    let angle = 0;
     let scale = 1;
     
-    const angleChange = Math.abs((angle - lastAngle) % 180);
-    if (velocity.length() > STRETCH_VELOCITY && (angleChange < ANGLE_THRESHOLD || (angleChange > 180 - ANGLE_THRESHOLD && angleChange < 180))) {
+    if (velocity.length() > STRETCH_VELOCITY) {
       scale = 1 + Math.min(Math.abs(velocity.length() - STRETCH_VELOCITY) * STRETCH_FACTOR, 2.5);
-    } else {
-      console.log(angleChange);
+      angle = new Vector2().copy(cursorPosition).sub(mousePosition).angle() * (180 / Math.PI);
     }
 
-    lastAngle = angle;
-   
     cursor.style.transform = `translate3d(${cursorPosition.x}px, ${cursorPosition.y}px, 0) rotate(${angle}deg) scale(${scale}, 1)`;
 
     requestAnimationFrame(animate);
